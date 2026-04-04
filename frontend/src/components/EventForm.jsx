7adb72152f5fb6f26dc01b05deeecf3e8c3431ec
yitingchung <emily850402@gmail.com>
2026-04-04 10:40:36 +1000

    fix page eventforms

--- frontend/src/components/EventForm.jsx
+++ frontend/src/components/EventForm.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect} from 'react';
 import { useAuth } from '../context/AuthContext';
 import axiosInstance from '../axiosConfig';
 import { Link } from 'react-router-dom';
