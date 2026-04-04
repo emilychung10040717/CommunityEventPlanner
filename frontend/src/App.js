ffe6ef3a6ac5307b1173a907213cf63d879a256e
yitingchung <emily850402@gmail.com>
2026-04-05 08:09:42 +1000

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Profile from './pages/Profile';
+import Profile from './pages/Profile';      //update
 import Events from './pages/Events';
 import ViewEvent from './pages/ViewEvent';
 import EditEvent from './components/EditEvent';
