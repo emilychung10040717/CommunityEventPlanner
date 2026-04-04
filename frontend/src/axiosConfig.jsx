c80122639c1dba6daf02b2d856bf333736c07540
yitingchung <emily850402@gmail.com>
2026-04-04 11:16:16 +1000

    update axisConfig.jsx

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,10 +1,30 @@
+// import axios from 'axios';
+
+// const axiosInstance = axios.create({
+//   //baseURL: 'http://localhost:5001', // local
+//   //baseURL: 'http://3.26.96.188:5001', // live
+//   baseURL: 'http://54.252.239.131:5001', // my public IP
+//   headers: { 'Content-Type': 'application/json' },
+// });
+
+// export default axiosInstance;
+
+
+// axiosConfig.js
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  //baseURL: 'http://localhost:5001', // local
-  //baseURL: 'http://3.26.96.188:5001', // live
-  baseURL: 'http://54.252.239.131:5001', // my public IP
-  headers: { 'Content-Type': 'application/json' },
+  baseURL: 'http://54.252.239.131:5001'
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  // 從 localStorage 或 context 取得 user 資料
+  const userData = localStorage.getItem('user'); 
+  if (userData) {
+    const { token } = JSON.parse(userData);
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default axiosInstance;
