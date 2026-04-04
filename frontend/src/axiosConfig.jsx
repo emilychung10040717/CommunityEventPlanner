b4125580955913da384bd2b0cbdba7abed3ae9da
yitingchung <emily850402@gmail.com>
2026-04-04 10:13:28 +1000

    update setting of backend and frontend

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
+  //baseURL: 'http://localhost:5001', // local
   //baseURL: 'http://3.26.96.188:5001', // live
+  baseURL: 'http://54.252.239.131:5001', // my public IP
   headers: { 'Content-Type': 'application/json' },
 });
 
