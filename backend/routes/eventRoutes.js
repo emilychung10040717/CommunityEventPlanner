de08fd46136ecab9f565991889ffdf8435af1c57
yitingchung <emily850402@gmail.com>
2026-04-05 00:39:41 +1000

    remove protect

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,11 +1,12 @@
 
 const express = require('express');
 const { getEvents, addEvent, updateEvent, deleteEvent } = require('../controllers/eventController');
-const { protect } = require('../middleware/authMiddleware');
+//const { protect } = require('../middleware/authMiddleware');
 const { getEventById } = require('../controllers/eventController');
 const router = express.Router();
 
-router.route('/').get(protect, getEvents).post(protect, addEvent);
-router.route('/:id').get(protect, getEventById).put(protect, updateEvent).delete(protect, deleteEvent);
+router.route('/').get(getEvents).post(addEvent);
+router.route('/:id').get( getEventById).put( updateEvent).delete( deleteEvent);
+//router.route('/:id').get(protect, getEventById).put(protect, updateEvent).delete(protect, deleteEvent);
 
 module.exports = router;
