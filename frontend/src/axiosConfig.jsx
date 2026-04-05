61e30802ee0d5b2739c691cfe3ab0fc2922269b2
yitingchung <emily850402@gmail.com>
2026-04-05 12:08:56 +1000

    eventform update

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,30 +2,8 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  //baseURL: 'http://3.26.96.188:5001', // live
-  baseURL: 'http://54.252.244.233',                //0504 remove":5001"
-  //baseURL: 'http://54.252.239.131:5001', // my public IP
+  baseURL: 'http://54.252.244.233:5001',     // live           
   headers: { 'Content-Type': 'application/json' },
 });
 
-export default axiosInstance;  //testtt
-
-
-// axiosConfig.js
-// import axios from 'axios';
-
-// const axiosInstance = axios.create({
-//   baseURL: 'http://54.252.239.131:5001'
-// });
-
-// axiosInstance.interceptors.request.use((config) => {
-//   // 從 localStorage 或 context 取得 user 資料
-//   const userData = localStorage.getItem('user'); 
-//   if (userData) {
-//     const { token } = JSON.parse(userData);
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-
-// export default axiosInstance;
+export default axiosInstance;  
